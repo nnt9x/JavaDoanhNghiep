a24f3bf8db3948d593900756eba509fad5e6fd2f
NNT9x <42613109+nnt9x@users.noreply.github.com>
2025-03-18 16:17:36 +0700

    -up

---
+++ src/main/java/day1b/Main3.java
@@ -0,0 +1,24 @@
+package day1b;
+
+import java.util.Scanner;
+
+public class Main3 {
+    public static void main(String[] args) {
+        int n;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("n = ");
+        n = scanner.nextInt();
+        if (n <= 0) {
+            System.out.println("Không hợp lệ!");
+        } else {
+            // Tim ước
+            System.out.print("Ước:");
+            for (int i = 1; i <= n; i++) {
+                if (n % i == 0) {
+                    System.out.print(i + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}

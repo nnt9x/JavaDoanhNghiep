aa71449b10c3c57904527ba818de1add91e5d379
NNT9x <42613109+nnt9x@users.noreply.github.com>
2025-03-19 10:41:42 +0700

    -up

---
+++ src/main/java/day2a/Main3.java
@@ -0,0 +1,12 @@
+package day2a;
+
+public class Main3 {
+    public static void main(String[] args) {
+        // Từ khoá continue
+        // In ra các số từ 1 đến 10 trừ số 3
+        for (int i = 1; i <= 10; i++) {
+            if(i == 3) continue;
+            System.out.print(i + "\t");
+        }
+}
+}
